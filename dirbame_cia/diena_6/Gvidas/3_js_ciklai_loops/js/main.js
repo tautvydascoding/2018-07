47fc6be2350f1ba1d938d81fee683152d0e8dcff
gvidas <gvidas.ulozevicius@gmail.com>
2018-08-06 17:00:48 +0300

    darbas klaseje

---
+++ dirbame_cia/diena_6/Gvidas/3_js_ciklai_loops/js/main.js
@@ -0,0 +1,36 @@
+// for (var i = 0; i < 50; i++) {
+//     console.log("karolis" + i);
+// }
+
+
+// 1 UZDUOTIS
+// naudojant FOR cikla, atspausdinti  "#" tiek, kad gautusi trikampio vaizdas
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+
+// var kintamasis = "";
+//
+// for (var i = 0; i < 6; i++) {
+//     kintamasis = kintamasis + "#";
+//     console.log(kintamasis);
+// }
+
+
+
+// ispresti 5! 15!
+
+var a = 0;
+for (var i = 0; i < 6; i++) {
+    a = a + i;
+}
+console.log("1+2+3+4+5:", a);
+
+var b = 0;
+for (var i = 0; i < 16; i++) {
+    b = b + i;
+}
+console.log("1+2+3...+15:", b);
