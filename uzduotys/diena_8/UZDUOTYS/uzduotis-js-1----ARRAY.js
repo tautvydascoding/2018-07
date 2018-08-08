774230b905264151b8b9826399b91eefea47aab8
Tautvydas <arttgd@gmail.com>
2018-08-08 08:36:36 +0300

    8

---
+++ uzduotys/diena_8/UZDUOTYS/uzduotis-js-1----ARRAY.js
@@ -0,0 +1,53 @@
+// ========js best practice & common mistakes (~2h)
+
+// -------- --Array / Masyvai  TEORIJA (~2h)--------------------
+	// UZDUOTIS
+	var prekiautojai = []; // empty array
+
+	prekiautojai[0] = "IKI";
+	console.log(  "prekybos centrai:", prekiautojai  );
+	
+	prekiautojai[1] = "Maxima";
+	prekiautojai[2] = "Lidl";
+	prekiautojai[3] = "Rimi";
+	prekiautojai[50] = "Cia Market";
+
+	// A) pervadinti pirma stalciu
+	prekiautojai[1] = "Barbora";
+
+	// B) sukeisti antro ir trecio stalciu reiksmes vietomis.
+	// !negalima rasyti rankomis String (nes duomenys kinta, ir jus nezinote koki duomenys bus po savaites)
+
+	// C) padaryti B) uzdaviniui f-ja sukeistiMasyvo2elementus(x, y)
+
+// -------- --Array / Masyvai  TEORIJA (~2h)--------------------
+names = ["Tomas", "Pietkus", 25, "VDU - inzinerija"];
+
+console.log(names);
+
+.length
+.join()
+.shift()
+.pop()
+
+
+
+// =========================================================
+// uzduotis  Array
+keiautojas = ["Tomas", "Pietkus", 25, "VDU - inzinerija"];
+
+// 1. i masyvo gala ideti elmenta "butelis vandens"
+// 2. i masyvo pradzia ideti elmenta "ziebtuvelis"
+// 3. istrinti 3-cia elmenta
+
+// 4. sukurti masyva (50 ilgumo) naudojant FOR ir uzpildyti visa skaiciais "0" (kiekvienas stalcius turi but lygus "0");
+
+
+// 5.1  sukurti masyva naudojant WHILE (50 ilgumo) ir uzpildyti visa skaiciumi 1;
+// 5.2: kas antra ^ masyvo elementa pakeisti i "3"
+// [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, ....
+// 5.3: kas 5-ta ^ masyvo elementa pakeisti i "9"
+// [1, 3, 1, 3, 9, 3, 1, 3, 1, 9, ....
+
+
+//=============bootstrap---sm-md-lg-xl (30-60m)==========
---
+++ uzduotys/diena_9/UZDUOTYS/uzduotis-js-4.1----ARRAY-copy-names-IN+.js
@@ -0,0 +1,64 @@
+console.log("labas");
+
+// =================taisyklingas Array copy  =================
+var arr1 = ['a', 'b', 'c'];  // Becomes arr1 = ['a', 'b', 'c']
+var arr2a = arr1.slice(0);   // Becomes arr2a = ['a', 'b', 'c'] - deep copy
+var arr2b = arr1.concat(  );   // Becomes arr2b = ['a', 'b', 'c'] - deep copy
+
+var arr2 =  arr1; // !!! Blogi  - sukurs susietaja kopija ir redaguojant keisis abu masyvai
+// ================= ========= ======== ========
+
+
+// UZDUOTIS
+
+ieskomiZmones = [2, 16, 17, 18, 19, 25];
+
+var names = ["Enriqueta", "Sybil", "Piper", "Anh", "Carmelo", "Regan", "Synthia", "Newton", "Norbert", "Krystyna", "Fidelia", "Christoper", "Lewis", "Jeromy", "Joy", "Lorri", "Owen", "Rosenda", "Devora", "Treva", "Leanora", "Meghann", "Jacqueline", "Bunny", "Tenisha", "Rico", "Clementina", "Samella", "Rico", "Sandi", "Efrain", "Tena", "Vivan", "Hiedi", "Naida", "Evette", "Shane", "Freida", "Marielle", "Wynona", "Cheree", "Gaston", "Aja", "Timika", "Jude", "Griselda", "Luise", "Rico", "Minh", "Warren"];
+
+var lastNames = ["Mcdowell", "Gates", "Mccall", "Cisneros", "Hancock", "Gaines", "Juarez", "Nolan", "Barajas", "Ware", "Orr", "Bell", "Donovan", "Rojas", "Stevenson", "Long", "Hays", "Gibson", "Meyer", "Sims", "Mcintosh", "Craig", "Haney", "Cunningham", "Hunt", "Montgomery", "Spears", "Cooke", "Gregory", "Mcknight", "Fernandez", "Hendrix", "Patton", "Bond", "Skinner", "Randolph", "Montes", "Guerra", "Bowen", "Potts", "Dyer", "Riley", "Rodgers", "Schroeder", "Ferguson", "Garrett", "Rush", "Moon", "Whitney", "Mcdaniel"];
+
+// 0 UZDUOTIS ----
+    var atlyginimas = 500;
+    var arPasikeite = false;
+    // UZDUOTIS
+    // A) patikrinti ar 'atlyginimas' padidejo ir panaudoti kintamaji
+    // 'arPasikeite'.rementis siuo kintamuoju, isvesti pranesima
+    // -" atlyginimas pasikeite" arba 'atlyginimas toks pats'
+    atlyginimas += 100;  // atlyginimas = atlyginimas + 100;
+    if ( atlyginimas > 500) {
+            console.log(" atlyginimas pasikeite");
+    } else {
+           console.log("  atlyginimas toks pats ");
+    }
+    // suprogramuoti patikrinima (su if), kad isvestu zinute
+    if ( atlyginimas > 500) {
+        arPasikeite = true;
+    }
+    // ar cia tiesa??
+    if ( arPasikeite ) {
+            console.log(" atlyginimas pasikeite");
+    } else {
+           console.log("  atlyginimas toks pats ");
+    }
+
+
+
+// 1A) surasti vardu masyve, kelintas zmogus yra "Rico" (surasti pirmaji; sunkes- surasti visus riko)
+// 1B) papildyti ^: jeigu tokio vardo neranda, isvesti VIENĄ pranesima "Nepavyko rasti...Bandykite kita zodi"
+
+
+// 2 UZDUOTIS (f-ja iekom stalciaus)
+// parasyti funkcija, kuriai davus iekoma zodi , ji suranda jo vieta masyve (stalciaus numeri) ir si   numeri grazina
+// eg: getStalciausNumeris( ieskomasTekstas)
+
+// uzduots:----------------------------
+// 3) rasti pavarde masyve esancio  zmogaus vardu "Freida" (pirmojo)
+// 4) rasti visu zmoniu vardu "Rico" pavardes
+// 5) Turime masyva su zmoniu nr.  ieskomiZmones = [2, 16, 17, 18, 19, 25];
+// A) atspausdinti visus numerius
+// B) isvesti ju pavardes ir vardus
+
+// var a = Math.random(); // 0 - 1
+// if ( a <= 0.1) {
+//     console.log("laimejai");
+// }
