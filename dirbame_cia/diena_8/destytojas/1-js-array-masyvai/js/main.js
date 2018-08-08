0ec462e25d2fe2ce31e68ea7c7eb9530fb5df949
Taurijus <tadas.valutis@gmail.com>
2018-08-08 11:42:19 +0300

    darbas

---
+++ dirbame_cia/diena_8/destytojas/1-js-array-masyvai/js/main.js
@@ -0,0 +1,19 @@
+console.log("Labas");
+
+// UZDUOTIS
+// 1.1 sukurti tuscia masyva "prekiautojai"
+// 1.2 uzpildyti masyva prekiaujanciomis imonemis pvz: senukai, ...
+// 1.3 isvesti i console/ekrana "prekiautojai" masyva
+
+var prekiautojai = []; // tuscias masyvas
+
+prekiautojai[0] = "AB senukai";
+prekiautojai[1] = "Ermitazas";
+prekiautojai[2] = "Moki vezi";
+prekiautojai[3] = "Vamsdziai ir KO";
+prekiautojai[50] = "Tomas ir Tomauskai";
+
+
+
+
+//
---
+++ uzduotys/diena_8/UZDUOTYS+++++/uzduotis-js-0----JS-best-practice.js
@@ -0,0 +1,3 @@
+// ========js best practice & common mistakes (~2h)
+
+ google w3 js best practice
