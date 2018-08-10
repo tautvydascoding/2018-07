77e638eca23f58aab5c7b4b6f9aeb5502ba24fdb
Martynas <marcius117@gmail.com>
2018-08-10 11:18:04 +0300

    js objektai

---
+++ dirbame_cia/diena_10/Martynas0810/UZDUOTYS/uzduotys--11.1-js--OBJEKTAI+++.js
@@ -0,0 +1,39 @@
+// PRISIMINTI TEORIJA
+var vardas = ""; // inicialisation (kintamojo kurimo metu, reiksmes priskyrimas)
+var zmogus; // undefined
+
+//===========================================
+// 1 UZDUOTIS:
+// sukurti mokinio objekta: vardas, pavarde, kelintokas, mokinio matematikos pazymiai (6, 5, 9 , 10, 8)
+// (ir priskirti reiksmes pvz: 'Jurgis', "Jurgauskas", 7)
+
+// 1.1 UZDUOTIS
+// atspauzdinti visus duomenis
+
+// UZDUOTIS 1.2
+// PAKEISTI : klase i sekancia
+// pakeisti: visus 6 pazymius
+// prideti nauja f-ja begu(), kuri isves i console 'bugu begu'
+// prideti nauja kintamaji: miestas = 'kaunas'
+// UZDUOTIS 1.3
+// isvesti pakeistus duomenis
+// paleisti f-ja "begu()"
+
+// 2 UZDUOTIS: sukurti 2-u objektus:
+// 1) tevas : vardas, pavarde, vaikas
+// 2) vaikas: klase, vardas
+// 3) priskirti tevo objekui (kintamajam) vaika
+
+
+// 3 UZDUOTIS
+// sukurti  objekto "darbuotojas" construktoriu. Objektas darbuotojas: nr, vardas, pavarde, alga
+
+//============3=pvz: construktorius =====
+// function Car(nr, c, ms) {
+//     this.numberOfDoors = nr;
+//     this.color = c;
+//     this.maxSpeed = ms;
+// }
+//
+// // objekto kurimas
+// var AudiTT = new Car(2, "red", 280);
---
+++ dirbame_cia/diena_10/Martynas0810/js objektai/js/main.js
@@ -0,0 +1,93 @@
+console.log("Labas");
+
+// PRISIMINTI TEORIJA
+var vardas = ""; // inicialisation (kintamojo kurimo metu, reiksmes priskyrimas)
+var zmogus; // undefined
+
+//===========================================
+console.log("//======= 1 UZDUOTIS: MOKINIO OBJEKTAS===============")
+// sukurti mokinio objekta: vardas, pavarde, kelintokas, mokinio matematikos pazymiai (6, 5, 9 , 10, 8)
+// (ir priskirti reiksmes pvz: 'Jurgis', "Jurgauskas", 7)
+
+var mokinioObjektas = {
+  vardas: "Martynas",
+  pavarde: "Martiniauskas",
+  klase: 12,
+  matPazymiai: [6, 5, 9, 10, 8],
+  begu: function () {
+    var i = "martynas bega";
+    return i
+  },
+
+};
+
+// atspauzdinti visus duomenis
+console.log(mokinioObjektas);
+// UZDUOTIS 1.2
+// PAKEISTI : klase i sekancia
+
+if (mokinioObjektas.klase >= 13) {
+  console.log("Jis baige mokykla, keliauja i studijas")
+}
+
+console.log(mokinioObjektas.klase);
+mokinioObjektas.klase = "1 kurso studentas";
+// jei butu 10tokas, tai butu buve geriau :
+// mokinioObjektas.klase++  // ir butu buves 11tokas
+
+// pakeisti: visus 6 pazymius ( priskiriu naujai)
+mokinioObjektas.matPazymiai = [7, 7, 4, 10, 10, 5];
+
+console.log(mokinioObjektas.matPazymiai);
+// prideti nauja f-ja begu(), kuri isves i console 'bugu begu'
+console.log(mokinioObjektas.begu());
+
+// prideti nauja kintamaji: miestas = 'kaunas'
+mokinioObjektas.miestas = "Kaunas"; // TAIP DARYTI GERIAU NEREIKIA
+console.log(mokinioObjektas);
+
+// UZDUOTIS 1.3
+// isvesti pakeistus duomenis
+console.log(mokinioObjektas.miestas, mokinioObjektas.matPazymiai, mokinioObjektas.klase, );
+// paleisti f-ja "begu()"
+
+// 2 UZDUOTIS: sukurti 2-u objektus:
+// 1) tevas : vardas, pavarde, vaikas
+// 2) vaikas: klase, vardas
+// 3) priskirti tevo objekui (kintamajam) vaika
+
+var tevas = {
+  vardas: " Benediktas",
+  pavarde: "Benediktauskas",
+  vaikas: {}
+};
+
+console.log(tevas);
+
+var vaikas = {
+  klase: 9,
+  vardas: "Tomas"
+};
+
+tevas.vaikas = vaikas;
+console.log(tevas.vaikas);
+
+// 3 UZDUOTIS
+// sukurti  objekto "darbuotojas" construktoriu. Objektas darbuotojas: nr, vardas, pavarde, alga
+
+function darbuotojas(nr, vardas, pavarde, alga) {
+
+};
+
+
+
+
+//============3=pvz: construktorius =====
+// function Car(nr, c, ms) {
+//     this.numberOfDoors = nr;
+//     this.color = c;
+//     this.maxSpeed = ms;
+// }
+//
+// // objekto kurimas
+// var AudiTT = new Car(2, "red", 280);
