f40f054ed9692c01632380b8c9a77d4bdd1e0040
Martynas <marcius117@gmail.com>
2018-08-14 18:51:18 +0300

    jqueris

--- dirbame_cia/diena_12/Martynas0814/jquery/js/main.js
+++ dirbame_cia/diena_12/Martynas0814/jquery/js/main.js
@@ -1,2 +1,26 @@
-console.log("Labas");
- 
+console.log("Labas rytc");
+// 1 uzduotis
+// su js sukurti <img elementa 200x200 dydzio
+// pasitikrinti su console
+
+var foto = $(" <img src= ''../kir.jpg' alt='cia pokemono foto'width='200' height='200' >");
+
+console.log(foto);
+
+// 1.1
+// su js sukurti <header> ir <footer>
+// ir ideti juos i body
+var header = $("<header/>");
+console.log(header);
+
+var footer = $("<footer/>");
+console.log(footer);
+
+$("body").prepend(header);
+
+$("body").append(footer);
+// 1.2
+// i <header> ir <footer> ideti <img> elementa
+
+$("header").append(foto);
+$("footer").append(foto);
