0c64d6155def86a408dca5648e4cdfffc941bc8e
Martynas <marcius117@gmail.com>
2018-08-17 11:11:54 +0300

    db funkcijos

---
+++ dirbame_cia/diena_14/Martynas0816/UZDUOTYS/uzduotys-3-PHP--sukurti-db-lib.php
@@ -0,0 +1,8 @@
+// UZDUOTIS
+
+// 1. sukurti faila:  db_functions.php
+// 2. parasysi PHP faile mysqli prisijungima prie DB
+// 3. suprogramuoti f-jas:
+// 4. getDoctor($nr)
+
+//susikurti nauja projekta ir issivesti daktara is DB naudojant getDoctor($nr) f-ja
