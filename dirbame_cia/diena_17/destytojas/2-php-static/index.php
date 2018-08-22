aca6d9940eff3a600a2c1ed204bd710fd9bb6f6a
Taurijus <tadas.valutis@gmail.com>
2018-08-22 17:14:14 +0300

    darbas

---
+++ dirbame_cia/diena_17/destytojas/2-php-static/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <!-- resposive design reikalingas -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+         <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
+        <link rel="stylesheet" href="libs/bootstrap/bootstrap.min.css">
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <!-- !!!! mano CSS failas visas patas zemiausias -->
+        <link rel="stylesheet" href="css/master.css?ver=1.0">
+
+        <!-- NOTE: jeigu neveikia:
+        1) ar failo pavadinimas geras
+        2) ar kelias geras?   (ar "/" teisingi; direktorija ar gere )
+        3) ar   rel="stylesheet"
+        4) ar link gale uzdalete ">"
+        5) ar tuo adresu nuejai internete (pvz: ar ne praeitos dienos projektas)
+    -->
+    </head>
+    <body>
+        <div class="container   ">
+
+            <h1> Isijunk Inspect->console </h1>
+
+            <?php
+                 function test () {
+                     static $a = 100;   // !!!! si eilute suveiks tik viena karta gyvenime
+                     echo $a . "<br />";
+                     $a -= 12; //$a = $a - 12;
+                 }
+                 test();
+                 test();
+                 test();
+                 test();
+
+                 // rekursija - tai yra f-ja, kuri iskviecia pati save
+                // UZDUOTIS
+                // iskviesti f-ja viena karta
+                // bet si f-ja turi suveikti `10 kartu
+                // (atspausdinti kelinta karta ji suveike)
+                function test2(){
+                    static $i = 1;
+                    $i++;
+                    if ($i < 11) {
+                        test2();
+                        echo "Paliedimo nr: $i     <br />";
+                    }
+                }
+                test2();
+             ?>
+
+
+
+
+
+        </div> <!--  end Container-->
+
+                <!--  =================================== -->
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <!-- !!! mano js failas pats zemiausias; visada failo -->
+        <script type="text/javascript" src="js/main.js"></script>
+
+    </body>
+</html>
