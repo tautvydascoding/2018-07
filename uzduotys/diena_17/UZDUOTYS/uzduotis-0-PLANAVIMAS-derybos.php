dd4854d5238925f1d60598b464a78a23b4c48470
Taurijus <tadas.valutis@gmail.com>
2018-08-23 11:21:25 +0300

    darbas

---
+++ uzduotys/diena_17/UZDUOTYS/uzduotis-0-PLANAVIMAS-derybos.php
@@ -0,0 +1,21 @@
+=====Komandine (derybos ir grafu teorija)======
+1. pasiskirstyti poromis: vienas puslapio uzsakovas kitas programuotojas
+
+2A 1). "Programuoto tikslas":
+    2A 1) gauti smulku darbu sarasa,
+
+    ##KOKI DUOMENYS TURI BUT ATVAIZDUOTI:
+        pvz.: home: meniu, 4 prekes pagrindines, kontaktai:, ...
+    ##Puslapio FUNKCIONALUMAS:
+        pvz.: prisijungimas,  prekes idejimas, ..., efektai: scrolinant issoka naujienos,
+
+    2A. 2) darbus sudelioti i Grafa (rutuliukus logine seka)
+    2A. 3)  Grafe  uzrasyti darbu padarymo laika
+
+2B. 1) "Uzsakovas" tikslas:
+        suvaidinti mazai besigaudanti zmogu, kuris nori uzsisakyti puslapi (puslapis jusu baigiamasis darbas)
+    2B. 2) (labai labai trumpai) paprasakoti apie savo puslapio ideja: kokiai auditorijai jis skirtas,  ka puslapis turetu duoti vartotojui (pvz: informacija kur mus rasti)
+    2B. 3) puslapio padarymo laika ir kaina
+
+3. apsikeisti rolemis
+4. destytojas patikrina grafa ir uzduotis ar ko nepraleidot
