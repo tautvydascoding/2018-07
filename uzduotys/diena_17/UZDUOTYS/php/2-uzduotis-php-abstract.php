2ce55b3a4a9163f6ce80d3fad54ee130a8b41890
Tautvydas <arttgd@gmail.com>
2018-08-24 08:13:40 +0300

    senas

---
+++ uzduotys/diena_17/UZDUOTYS/php/2-uzduotis-php-abstract.php
@@ -0,0 +1,41 @@
+
+
+    // uzduotis:
+    // 1) sukurti interface "Edit" :
+    //  1.1) kuri turi funkcijas: (kurios nera aprasytos (privalomos)!
+        // changeName($a)
+        // getName()
+        // isLogin()
+        // NOTE: Ar interface gali tureti kintamuosius????
+    // 2) sukurti (abstrakcia) klase "User" :
+        // 0) abstrakti kalse - NEGALI TURETI ABSTRAKCIU KINTAMUJU (BET PAPRASTUS GALI)
+        // A) sukurti kintamuosius:
+            // name, password , createdDate, lastLogin
+        // B) privaloma funkcija "Loguot()"
+        // C) neprivaloma funkcija clearlastLoginTime()
+    // 3) sukurti klase Admin :
+        // A)  kuri panaudoja Edit ir User ~klases (implementuoti ir paveldeti)
+    // 4)
+         // A) issivesti username,
+         // B) pakeisti username
+         // C) issivesti username
+         // 5)  panaudoti login funkcija
+         // A)  panaudoti login funkcija , pakeitus username
+
+
+
+
+    // uzduotis:
+    // 1) sukurti interface "Edit" : kuri turi funkcijas: (kurios nera aprasytos (privalomos)!
+        // 0) Ar gali tureti kintamuosius????
+        // changeName($a)
+        // isLogin()
+        // getName()
+    // 2) sukurti (abstrakcia) klase "User" :
+        // 0) abstrakti kalse - NEGALI TURETI ABSTRAKCIU KINTAMUJU (BET PAPRASTUS GALI)
+        // A) kuri turi neprivalomus kintamuosius:
+            // name, password , createdDate, lastLogin
+        // B) privaloma funkcija "Loguot()"
+        // C) neprivaloma funkcija clearlastLoginTime()
+    // 3) sukurti klase Admin :
+        // A)  kuri panaudoja Edit ir User ~klases (implementuoti ir paveldeti)
---
+++ uzduotys/diena_17/UZDUOTYS/uzduotys-1-Pasikartojimas-For-Pagrindu-testui2.php
@@ -0,0 +1,14 @@
+// ---- -PASIKARTOJIMAS PROGRAMAVIMO PAGRINDU TESTUI-----
+// 1) sukurti masyva : 1981, 1980, 1975, 1995, 1999
+// 1.1) atspausdinti masyva naudojant FOR cikla, While cikla ir FOREACH cikla
+// 1.4 Atspausdinti kas antra data
+// 1.5 Atspasudinti visas datas nuo 3-cios (iskaitant 3-cia)
+
+
+// 2) sukurti matrica:
+// Tomas, 1981, Buhalteris
+// Ona 1980 Programuotoja
+// Petras, 1975, Direktorius
+// 2.1) atspausdinti su FOR ciklu
+// 2.2) atspausdinti su FOR FOR ciklu
+// 2.3) atspausdinti su FOREACH
