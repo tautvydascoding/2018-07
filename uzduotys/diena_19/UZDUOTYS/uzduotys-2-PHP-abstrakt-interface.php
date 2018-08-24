c6078b3b7aa0020334c2dc3ae0353ed0562115ef
Tautvydas <arttgd@gmail.com>
2018-08-24 08:22:33 +0300

    19

---
+++ uzduotys/diena_19/UZDUOTYS/uzduotys-2-PHP-abstrakt-interface.php
@@ -0,0 +1,23 @@
+<?php
+    // uzduotis:
+    // 1) sukurti interface "Edit" :
+    //  1.1) kuri turi funkcijas: (kurios nera aprasytos (privalomos)!
+        // changeName($a)
+        // getName()
+        // isLogin()
+        // NOTE: Ar interface gali tureti kintamuosius????
+    // 2) sukurti (abstrakcia) klase "User" :
+        // 0) abstrakti kalse - NEGALI TURETI ABSTRAKCIU KINTAMUJU (BET PAPRASTUS GALI)
+        // A) sukurti kintamuosius:
+            // name, password , createdDate, lastLogin
+        // B) abstrakti funkcija "Loguot()"
+        // C) NE abstrakti funkcija clearlastLoginTime()
+    // 3) sukurti klase Admin :
+        // A)  kuri panaudoja Edit ir User ~klases (implementuoti ir paveldeti)
+    // 4)
+         // A) issivesti username,
+         // B) pakeisti username
+         // C) issivesti username
+
+
+     // NOTE: abstrakti klase - NEGALI TURETI ABSTRAKCIU KINTAMUJU (BET PAPRASTUS GALI)
