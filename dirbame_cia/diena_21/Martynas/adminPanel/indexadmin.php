55daa031042a6ca3c03d6634581fc7016e7337b8
Martynas <marcius117@gmail.com>
2018-08-27 11:13:24 +0300

    ajax ir login veikia

--- dirbame_cia/diena_21/Martynas/adminPanel/indexadmin.php
+++ dirbame_cia/diena_21/Martynas/adminPanel/indexadmin.php
@@ -1,5 +1,6 @@
 <?php
-include("Aheader.php")
+include("Aheader.php");
+
 ?>
 
 <div class='mainText'>
--- dirbame_cia/diena_21/Martynas/login/index.php
+++ dirbame_cia/diena_21/Martynas/login/index.php
@@ -8,10 +8,20 @@ include("Lhead.php");
 <section class="main-container">
 
   <div class="main-wraper">
-    <h2>Home</h2>
+    <h2>UNDO STUDIJA ADMINISTRACIJA</h2>
+      
 <?php
 if (isset($_SESSION['u_id'])) {
-  echo "Login success";
+  echo "<h3>Is cia galite valdyti tam tikras puslapio funkcijas, tokias kaip:</h3> <br>
+  <ol>
+      <li> Socialines ikoneles (jas prideti, naujinti ar istrinti).</li>
+      <li> Karuseles paveikselius (juos prideti ir istrinti).</li>
+      <li>Home teksta (naujinti ar istrinti).</li>
+      <li>Matyti asmenu, kurie parase zinutes informacija, bei ja valdyti.</li>
+      <li>Matyti prisiregistravusiu useriu informacija ir ja valdyti</li>
+  </ol>
+  
+  ";
 }
 ?>
 
